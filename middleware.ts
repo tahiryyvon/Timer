778b10a0e07103784b77c4970875c1efc6d05c5b
tahiryyvon <tahiry.yvon@gmail.com>
2025-12-22 22:07:03 +0300

    Fix path

--- middleware.ts
+++ middleware.ts
@@ -14,16 +14,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(de|en|fr)/:path*',
-
-    // Enable redirects that add missing locales
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|_vercel|.*\\..*).*)'
-  ]
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
